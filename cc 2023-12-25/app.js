af9bd4ddbc7da591d44c42a1840447db602c58af
Rodrigo Blejman <rodrigo.blej@gmail.com>
2023-12-25 16:19:16 -0800

    kata n4 solved

--- cc 2023-12-25/app.js
+++ cc 2023-12-25/app.js
@@ -21,3 +21,54 @@ console.log("Answer: AAAAAA");
 // function repeatStr (num, string) {
 //     return string.repeat(num);
 //   }
+
+// Kata n2
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
+
+console.log(greet("Rodrigo"));
+
+//Kata n3
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  return -number;
+}
+
+console.log(opposite(4), -4);
+
+//Kata n4
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+function century(year) {
+  if (year >= 1000) {
+    return Math.floor(year / 100) + 1;
+  } else {
+    return 1;
+  }
+}
+
+console.log(century(1700));
+
+// A better solution
+// const century = year => Math.ceil(year/100)
