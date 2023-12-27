4ae6a65834c3edd2f1b9f2db1c40622f86778d0f
Rodrigo Blejman <rodrigo.blej@gmail.com>
2023-12-26 18:34:20 -0800

    kata n2 solved

--- cc 2023-12-26/app.js
+++ cc 2023-12-26/app.js
@@ -23,3 +23,21 @@ console.log(abbrevName("Sam Harris"), "S.H");
 //     return name.split(' ').map(i => i[0].toUpperCase()).join('.')
 
 // }
+
+// Kata n2 <---------------
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+
+// For example (Input -> Output):
+
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+function summation(num) {
+  let accum = 0;
+  for (let index = 1; index <= num; index++) {
+    accum += index;
+  }
+  return accum;
+}
+
+console.log(summation(8), 36);
