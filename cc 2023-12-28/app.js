e637138da31a340315c6f853398be14f95c6149c
Rodrigo Blejman <rodrigo.blej@gmail.com>
2023-12-28 19:13:10 -0800

    cc 2023-12-28

---
+++ cc 2023-12-28/app.js
@@ -0,0 +1,11 @@
+// Kata n1 <-----
+
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string) {
+  return string.split(" ");
+}
+
+console.log(stringToArray("Robin Singh"));
