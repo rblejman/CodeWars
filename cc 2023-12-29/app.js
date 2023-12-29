3f5b7c932ae98a57f6da0fa8817dd8fc9609bf75
Rodrigo Blejman <rodrigo.blej@gmail.com>
2023-12-29 15:19:09 -0800

    kata n2 solved

--- cc 2023-12-29/app.js
+++ cc 2023-12-29/app.js
@@ -20,3 +20,16 @@ console.log(shortcut("how are you today?"), "hw r y tdy?");
 // function shortcut(string){
 //     return string.replace(/[aeiou]/g,'')
 //   }
+
+// Kata n2 <---
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+function nameShuffler(str) {
+  return str.split(" ").reverse().join(" ");
+}
+
+console.log(nameShuffler("john McClane"), "McClane john");
