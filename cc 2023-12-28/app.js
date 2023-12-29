bb161daa1acdafc9f5130b7e466184d6917e7ee1
Rodrigo Blejman <rodrigo.blej@gmail.com>
2023-12-28 19:23:07 -0800

    kata n2 solved

--- cc 2023-12-28/app.js
+++ cc 2023-12-28/app.js
@@ -9,3 +9,23 @@ function stringToArray(string) {
 }
 
 console.log(stringToArray("Robin Singh"));
+
+// Kata n2 <----
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+function findAverage(array) {
+  if (array.length !== 0) {
+    let average =
+      array.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+      ) / array.length;
+    return average;
+  } else {
+    return 0;
+  }
+}
+
+console.log(findAverage([5, 5, 5]));
