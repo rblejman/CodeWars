a8d2c97fc16a0ba718cc5b110d38d7416124b96f
Rodrigo Blejman <rodrigo.blej@gmail.com>
2024-01-02 16:20:38 -0800

    kata 3 solved

--- cc 2024-01-02/app.js
+++ cc 2024-01-02/app.js
@@ -47,3 +47,26 @@ console.log(shortenToDate("Friday May 2, 7pm"), "Answer: Friday May 2");
 
 // A shorter version could be
 // return longDate.split(",")[0];
+
+// Kata 3 <---
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2) {
+  return (flower1 % 2 === 0 && flower2 % 2 === 0) ||
+    (flower1 % 2 === 1 && flower2 % 2 === 1)
+    ? false
+    : true;
+  //   if (flower1 % 2 == 0 && flower2 % 2 == 0) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+}
+console.log(lovefunc(2, 3), true);
+console.log(lovefunc(2, 2), false);
+console.log(lovefunc(119, 663), false);
+
+// A clever way to solve it:
+//  return flower1 % 2 !== flower2 % 2;
