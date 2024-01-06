d05e26813a83c99899106a55515209c262dc2f94
Rodrigo Blejman <rodrigo.blej@gmail.com>
2024-01-06 12:32:15 -0800

    kata 1 solved

--- cc 2024-01-06/app.js
+++ cc 2024-01-06/app.js
@@ -1,1 +1,29 @@
 // Kata 1 <----
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, cVal) => acc + cVal, 0);
+}
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]), 21);
+
+// // Kata 2 <----------------------
+// Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+// The geese are any strings in the following array, which is pre-populated in your solution:
+
+//   ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+// For example, if this array were passed as an argument:
+
+//  ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+// Your function would return the following array:
+
+// ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+// The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated
+
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+  // return an array containing all of the strings in the input array except those that match strings in geese
+}
