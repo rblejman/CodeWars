19346496c49df5a96166ab147f78b1945f38f065
Rodrigo Blejman <rodrigo.blej@gmail.com>
2024-01-13 14:31:11 -0800

    kata 1 solved

--- cc 2024-01-11/app.js
+++ cc 2024-01-11/app.js
@@ -1,1 +1,21 @@
 // Kata 1 <---
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+function findSmallestInt(args) {
+  let newArr = args.sort((a, b) => a - b);
+  return newArr[0];
+}
+console.log(findSmallestInt([78, 56, 232, 12, 8]), "Answer:", 8);
+
+/*
+A better solution:
+findSmallestInt(args) {
+    return Math.min(...args)
+  }
+
+*/
