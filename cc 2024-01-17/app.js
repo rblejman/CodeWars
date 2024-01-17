2bb169559d830f87e280041e0077911aea455d8f
Rodrigo Blejman <rodrigo.blej@gmail.com>
2024-01-17 10:18:16 -0800

    kata 1 solved

---
+++ cc 2024-01-17/app.js
@@ -0,0 +1,29 @@
+// Kata 1 <--------
+/*
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+Input constraints:
+
+0 <= h <= 23
+0 <= m <= 59
+0 <= s <= 59
+*/
+
+function past(h, m, s) {
+  if (h == 0 && m == 0 && s == 0) {
+    return 0;
+  } else {
+    return (h * 3600 + m * 60 + s) * 1000;
+  }
+}
+
+console.log(past(0, 1, 1), 61000);
+console.log(past(1, 1, 1), 3661000);
