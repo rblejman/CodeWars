5318af7e714012428fba6775d6956de6769f2f41
Rodrigo Blejman <rodrigo.blej@gmail.com>
2024-01-22 09:44:38 -0800

    kata 2 solved

--- cc 2024-01-22/app.js
+++ cc 2024-01-22/app.js
@@ -14,3 +14,41 @@ function paperwork(n, m) {
 }
 
 console.log(paperwork(5, 5), "answer:", 25);
+
+// Kata 2 <----------------------
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+function countPositivesSumNegatives(input) {
+  if (!input == null || !input.length == 0) {
+    let result = [];
+    result.push(input.reduce((acc, val) => acc + (val > 0 ? 1 : 0), 0));
+    result.push(input.reduce((acc, val) => acc + (val <= 0 ? val : 0), 0));
+    return result;
+  } else {
+    return [];
+  }
+}
+
+console.log(
+  countPositivesSumNegatives([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+  ]),
+  "answer:",
+  [10, -65]
+);
+
+console.log(
+  countPositivesSumNegatives([
+    0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14,
+  ]),
+  "answer",
+  [8, -50]
+);
