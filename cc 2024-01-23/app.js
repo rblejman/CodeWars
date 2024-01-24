7f5447ebd2f5705a7bac6e73e0e670ff490aba02
Rodrigo Blejman <rodrigo.blej@gmail.com>
2024-01-23 19:44:01 -0800

    kata 3 solved

--- cc 2024-01-23/app.js
+++ cc 2024-01-23/app.js
@@ -20,3 +20,28 @@ function fakeBin(x) {
     .join("");
 }
 console.log(fakeBin("45385593107843568"), "01011110001100111");
+
+/*  Another Option:
+return x.replace(/\d/g, d => d < 5 ? 0 : 1);
+*/
+
+// Kata 3 <----
+/*
+You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+For example(Input --> Output):
+
+10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ 1 --> [1]
+*/
+function monkeyCount(n) {
+  let array = [];
+  for (let index = 1; index <= n; index++) {
+    array.push(index);
+  }
+  return array;
+}
+
+console.log(monkeyCount(5));
