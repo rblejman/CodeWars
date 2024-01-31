6b02c11e5ac6b0c3d508ccc100e2718b0ea0b26f
Rodrigo Blejman <rodrigo.blej@gmail.com>
2024-01-30 21:38:10 -0800

    kata 2 solved

--- cc 2024-01-27/app.js
+++ cc 2024-01-27/app.js
@@ -21,3 +21,23 @@ function countBy(x, n) {
 }
 
 console.log(countBy(2, 5));
+
+// kata 2 <--
+/*
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+Good Luck!
+*/
+
+function doubleChar(str) {
+  return str
+    .split("")
+    .map((e) => e + e)
+    .join("");
+}
+
+console.log(doubleChar("abcd"), "aabbccdd");
