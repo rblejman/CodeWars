b2096036d592c07ff12d93a647e8d9975749f353
Rodrigo Blejman <rodrigo.blej@gmail.com>
2024-02-07 20:58:25 -0800

    kata 1 solved

---
+++ cc 2024-02-07/app.js
@@ -0,0 +1,10 @@
+// Kata 1 <<----------
+var min = function (list) {
+  list.sort((a, b) => a - b);
+  return list[0];
+};
+
+var max = function (list) {
+  list.sort((a, b) => b - a);
+  return list[0];
+};
