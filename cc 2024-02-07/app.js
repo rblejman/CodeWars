16ceb3582146a3fa6aef629f2f7168c96587e6b2
Rodrigo Blejman <rodrigo.blej@gmail.com>
2024-02-11 11:11:03 -0800

    kata 2 solution

--- cc 2024-02-07/app.js
+++ cc 2024-02-07/app.js
@@ -10,3 +10,26 @@ var max = function (list) {
 };
 
 //Kata 2 <--
+/*
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation.
+
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+*/
+
+function sumArray(array) {
+  array.sort((a, b) => a - b);
+  array.pop();
+  array.shift();
+  console.log(array);
+  return array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+}
+console.log(sumArray([6, 2, 1, 8, 10]), 16);
