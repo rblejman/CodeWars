ec681704616dee6f29b33ad899121e8f39ad269f
Rodrigo Blejman <rodrigo.blej@gmail.com>
2024-02-16 16:53:30 -0800

    feb 16 kata 2 solved

--- cc 2024-02-16/app.js
+++ cc 2024-02-16/app.js
@@ -29,3 +29,19 @@ function getGrade(s1, s2, s3) {
 }
 
 console.log(getGrade(95, 90, 93), "A");
+
+// kata 2
+/*
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son 
+(or in how many years he will be twice as old). The answer is always greater or equal to 0,
+ no matter if it was in the past or it is in the future.
+*/
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - sonYearsOld * 2);
+}
+console.log(twiceAsOld(36, 7), 22);
