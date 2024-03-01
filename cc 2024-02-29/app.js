c379d959a1a0e5493bfdf934ecf7376debb2bb85
Rodrigo Blejman <rodrigo.blej@gmail.com>
2024-02-29 22:12:23 -0800

    cc 2024-02-29 filter ideas

---
+++ cc 2024-02-29/app.js
@@ -0,0 +1,27 @@
+// kata 1
+
+/*
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+
+
+*/
+function well(x) {
+  let res = x.filter((idea) => idea == "good");
+  if (res.length >= 1 && res.length <= 2) {
+    return "Publish!";
+  } else if (res.length >= 3) {
+    return "I smell a series!";
+  } else {
+    return "Fail!";
+  }
+}
+
+console.log(well(["bad", "bad", "bad"]), "Fail!");
+console.log(well(["good", "bad", "bad", "bad", "bad"]), "Publish!");
+console.log(
+  well(["good", "bad", "bad", "bad", "bad", "good", "bad", "bad", "good"]),
+  "I smell a series!"
+);
