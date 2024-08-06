f20b28b277f5a25f44f601391de5f07da321a8aa
Rodrigo Blejman <rodrigo.blej@gmail.com>
2024-08-05 19:38:31 -0700

    kata n2

--- cc 2024-08-05/app.js
+++ cc 2024-08-05/app.js
@@ -7,3 +7,15 @@ Complete the solution so that it reverses the string passed into it.
 function solution(str) {
   return str.split("").reverse().join("");
 }
+
+/*
+The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+For example:
+
+1.08 --> 30
+*/
+
+function cockroachSpeed(s) {
+  return Math.floor(s * 27.777778);
+}
